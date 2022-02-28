3f187764e78d6308697977efec3f44c97c11402c
shruthiedke <100557706+shruthiedke@users.noreply.github.com>
2022-02-28 13:20:05 +0530

    Add files via upload

---
+++ shruthi.py
@@ -0,0 +1,53 @@
+
+
+
+# print("hello\world")
+# print("hello/world")
+# print("hello\\world")
+# print("hi \ankith")
+# print("hii\\shruthi")
+# print("heyy\t sony")
+# print("\t hiiisony")
+# print("it is\r summer")
+# print("hello\rpy")
+# print("hiii\ashruthi")
+# print("hellooo\nsony")
+# print("hello\t shruthi")
+# print("heee  \t i'm shruthi")
+# print("hiii\ t  i am shruthi")
+# a=3
+# print(3)
+# b=4
+# print(b)
+# print(a,
+# n="shruthi"
+# print(n)
+# print("my name is:,n")
+from operator import truediv
+from pyexpat import native_encoding
+
+
+# s=input()
+# print('my name is:',s)
+# n=1
+# print(type(n))
+# p=10.3
+# print(p,type(p))
+# s=input("67")
+# print('my rule number is:
+# r=input(12.3)
+# print("my decimal is:,r")
+# d=float(12.3)
+# print(d,type(d))
+s=input('sony')
+print(s)
+r="naveen"
+print(r,type(r))
+a=input('navin')
+print(a,type(a))
+a="23.4"
+print(a)
+a=input
+z="32.4"
+a=float(z)
+print(a,type(a))
